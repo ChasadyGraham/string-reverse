dcbf619df6e900c3466622ff5b42fd97f09e6f64
pebblessurpreme <pebblessurpreme@msn.com>
2017-06-23 16:22:08 -0400

    changes switch js

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 var $textInput = $("#textInput"),
     $button = $("#doAction"),
     $display = $("#display");
+    $options = $("input[type=radio]");
+    method = "reverse";
+		
+$options.on("click", function() {
+	method = $(this).val();/*this is just assign a value to the button being clicked */
+	
+	
+}	)
 
-$button.on("click", function(){
-  let word = $textInput.val();
-  let reversedWord = word.split("").reverse().join("");
+
+$button.on("click", function() {
+       let word = $textInput.val();
+	switch(method) {
+    
+		case "Random":
+  let randomisedWord = word.split("").sort(function(a, b) {
+		return 0.5 - Math.random()
+	});
+   $display.text(randomisedWord.join(""));
+        break;
+			
+    case "Alphabetize":
+  let alphabetizedWord = word.split("").reverse().join("");
+   $display.text(alphabetizedWord);
+        break;
+    
+		case "Reverse":
+   let reversedWord = word.split("").reverse().join("");
+   $display.text(reversedWord);
+break;
+}});
   
-  $display.text(reversedWord);
-});
+ 
+
+
+
+
+
