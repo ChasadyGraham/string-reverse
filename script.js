8591a8a5210ca9e9007a8d28ab6d7ad250e7c73d
pebblessurpreme <pebblessurpreme@msn.com>
2017-06-23 14:16:02 -0400

    final

---
+++ script.js
@@ -0,0 +1,10 @@
+var $textInput = $("#textInput"),
+    $button = $("#doAction"),
+    $display = $("#display");
+
+$button.on("click", function(){
+  let word = $textInput.val();
+  let reversedWord = word.split("").reverse().join("");
+  
+  $display.text(reversedWord);
+});
